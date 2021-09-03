101b2a7e5c948be9c581ed19c209007e8f008c0f
lottejd <90038404+lottejd@users.noreply.github.com>
2021-09-03 13:34:40 +0200

    Add hello world method and tests (#1)

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -1,13 +1,24 @@
 using System;
 using Xunit;
+using System.IO;
 
 namespace MyApp.Tests
 {
-    public class UnitTest1
+    public class MainTests
     {
         [Fact]
-        public void Test1()
+        public void Main_prints_hello_world()
         {
+            //Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            //Act
+            Program.Main(new string[0]);
+
+            //Assert
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Equal("Hello World!",output);
 
         }
     }
