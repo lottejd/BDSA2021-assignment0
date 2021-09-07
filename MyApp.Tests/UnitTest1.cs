e6a1e43fa3ea22e8776d68c94f0ecd26eda4b7c1
Lotte <lottejd@gmail.com>
2021-09-07 13:50:05 +0200

    Tested and implemented IsLeapYear

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -4,7 +4,7 @@ using System.IO;
 
 namespace MyApp.Tests
 {
-    public class MainTests
+    public class ProgramTests
     {
         [Fact]
         public void Main_prints_hello_world()
@@ -20,6 +20,37 @@ namespace MyApp.Tests
             var output = writer.GetStringBuilder().ToString().Trim();
             Assert.Equal("Hello World!",output);
 
+        }
+
+        [Theory]
+        [InlineData(4)] //First rule
+        [InlineData(400)] //Third rule
+        public void IsLeapYear_returns_true_on_correct_years(int year)
+        {
+            //Arrange
+
+            //Act
+            var output = Program.IsLeapYear(year);
+
+            //Assert
+            Assert.True(output);
+            
+        }
+
+
+        [Theory]
+        [InlineData(1)] //First rule
+        [InlineData(100)] //Second rule
+        public void IsLeapYear_returns_false_on_incorrect_years(int year)
+        {
+            //Arrange
+
+            //Act
+            var output = Program.IsLeapYear(year);
+
+            //Assert
+            Assert.False(output);
+            
         }
     }
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -8,5 +8,15 @@ namespace MyApp
         {
             Console.WriteLine("Hello World!");
         }
+
+        public static bool IsLeapYear(int year)
+        {
+            if(year % 4 == 0)
+            {
+                if(year % 100 != 0 || year % 400 == 0) return true;
+            }
+            
+            return false;
+        }
     }
 }
